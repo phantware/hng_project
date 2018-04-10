87255580007507a5c2d25d240adee35d1a388217
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2018-04-10 18:31:47 +0100

    This is my first website

---
+++ index.php
@@ -0,0 +1,52 @@
+
+<!DOCTYOPE html>
+<html lang="US-en">
+  <head>
+    <title>FIGMA</title>
+    <meta charset="utf-8">
+    <style id="webmakerstyle">
+body{
+  background-color: #6576A1;
+}
+h1{
+  font-size:32px;
+  color: #fff;
+  text-align: center;
+}
+p{
+ font-size:22px;
+  color: #fff;
+  text-align: center; 
+}
+img{
+  height: 381px;
+  width: 435px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  border-radius: 50%;
+}
+
+</style>
+  </head>
+  <body>
+    <header>
+      <h1>
+        PHANTWARE COMPANY
+      </h1>
+      <img src="img/signout.jpg" alt="phantware photo">
+      </header>
+    <section>
+      <h1>
+         ISMAIL JAMIU BABATUNDE
+      </h1>
+      <p>
+        I AM A WEB DEVELOPER, COMPUTER ENGINEER AND BUSINESS CONSULTANT
+       
+      </p>
+       </section>
+       <h1><?=date('d-m-Y H:i:s', time() )?></h1>
+       <h2></h2>
+      
+  </body>
+</html>
